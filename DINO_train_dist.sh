8fd8ae2d14e3a5e7c91e3a80ee15ba25f3412d6c
Abbas Mammadov <abbas.memmedov370@gmail.com>
2022-12-23 02:27:18 +0900

    final

--- DINO_train_dist.sh
+++ DINO_train_dist.sh
@@ -1,8 +1,8 @@
 coco_path=$1
 python3 -m torch.distributed.launch --nproc_per_node=4 main.py \
-	--output_dir logs/exp_rail/ -c rail/DINO_4scale_rail.py --coco_path $coco_path \
+	--output_dir logs/exp_rail/ -c config/DINO/DINO_4scale.py --coco_path $coco_path \
 	--pretrain_model_path checkpoints/checkpoint0023_4scale.pth \
 	--finetune_ignore label_enc.weight class_embed \
 	--options num_classes=22 dn_scalar=100 embed_init_tgt=TRUE \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
-	dn_box_noise_scale=1.0
+	dn_box_noise_scale=1.0 num_classes=22 dn_labelbook_size=22\
